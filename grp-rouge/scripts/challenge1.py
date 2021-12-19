f05deb2cbc200830c47780baa6f148b16326527f
hugovanhille <hugovanhille59@orange.fr>
2021-12-19 21:54:16 +0100

    version finale commentée

--- grp-rouge/scripts/challenge1.py
+++ grp-rouge/scripts/challenge1.py
@@ -7,59 +7,52 @@ from geometry_msgs.msg import Twist
 direction = 0
 timer = 0
 
-# move(data) is the function that will publish the directions to move the robot
-# depending on the 'direction' variable modified in the other functions 
+# la fonction move permet de faire avancer le robot ou le faire tourner en cas de présence d'un obstacle sur son chemin
 def move(data):
     global direction
     global timer
-    move_cmd = Twist()
-    if direction == 2 :
-        move_cmd.linear.x = 0.0
-        move_cmd.angular.z = -0.6
-    elif direction == 1 :
-        move_cmd.linear.x = 0.0
-        move_cmd.angular.z = 0.6
-    else :
-        move_cmd.linear.x = 0.5
-        move_cmd.angular.z = 0.0
-    pub.publish(move_cmd)
+    move = Twist()
+
+    if direction == 2 :                 #Tourner le robot dans le sens horaire
+        move.linear.x = 0.0
+        move.angular.z = -0.6
+    elif direction == 1 :               #Tourner le robot dans le sens antihoraire
+        move.linear.x = 0.0
+        move.angular.z = 0.6
+    else :                              #Avancer le robot
+        move.linear.x = 0.5
+        move.angular.z = 0.0
+
+    pub.publish(move)
     if timer != 0 :
-        timer -= 1
+        timer = timer-1
 
-# choix(liste_obstacles) is called when a collision is detected and will 
-# analyse the environnement to choose in with direction it is better to
-# turn to avoid the collision
-
-# detect_collision(liste_obstacles) will choiceide if there is a collision danger 
-# with one of the liste_obstacles detected and will then execute choix
-
-# callback(data) uses the data provided by the topic from the 
-# laser to create a list of points that are liste_obstacles seen by the laser
+# la fonction callback permet de détecter les obstacles à proximité du robot et de définir la direction à prendre en cas de présence d'un obstacle sur le chemin du robot
 
 def callback(data):
     global direction
     global timer
 
-    liste_obstacles = []
+    liste_obstacles = []                
     min= data.angle_min
-    for dist in data.ranges :
+    for dist in data.ranges :                  #Boucles permettant de référencer tous les obstacles à proximité du robot
         if 0.2 < dist and dist < 0.6 :
             aPoint= [ 
-                math.cos(min) * dist, 
+                math.cos(min) * dist,               #Calcul des coordonées de l'obstacle
                 math.sin(min) * dist
             ]
-            liste_obstacles.append(aPoint)
+            liste_obstacles.append(aPoint)          #Ajout de l'obstacle dans la liste
         min+= data.angle_increment
     
 
     if timer == 0 : 
 
         direction = 0
-        for obstacle in liste_obstacles : 
-            if 0.2 < obstacle[0] and obstacle[0] < 0.6 and abs(obstacle[1]>0.3):
+        for obstacle in liste_obstacles :                                           #Pour chaque obstacle
+            if 0.2 < obstacle[0] and obstacle[0] < 0.6 and abs(obstacle[1]>0.3):            #on regarde s'il est situé sur le chemin de notre robot
                     choice = 0
                     for obstacle in liste_obstacles :
-                        if 0.2 < obstacle[0] and obstacle[0] < 0.5 and abs(obstacle[1]>0.2):
+                        if 0.2 < obstacle[0] and obstacle[0] < 0.5 and abs(obstacle[1]>0.2):    #Si oui on définit la direction qui est la meilleur:
                             choice += obstacle[1]
                     if choice >= 0 :
                         direction = 2
@@ -68,8 +61,6 @@ def callback(data):
                     timer = 20
 
 
-# main_prog() contains the main structure of the program : 
-# callbacks from laser scans and a regular publishing to move the robot
 
 pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
 rospy.init_node('challenge1', anonymous=True)
